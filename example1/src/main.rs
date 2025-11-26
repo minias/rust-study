7d08bcbcbce24964ae4f587db126a52d917a3038
minias <minias@nsmg21.com>
2025-11-26 12:48:33 +0800

    example1 lib.rs 반복문 및 로직 최적화, 공통함수 분리

--- example1/src/main.rs
+++ example1/src/main.rs
@@ -4,10 +4,30 @@
 // 위의 명령어 입력후 Cargo.toml이 자동생성된다.
 use exam1::max_even_sum;
 
+/// 이것은 러스트 문서화를 위한 내용입니다.
+///
+/// 핵심로직은 lib.rs에 있는 max_even_sum() 함수입니다.
+///
+/// ```
+/// fn main() {
+///     // cli에서 인자값을 3개 이상 받지 못하면 종료
+///     let args: Vec<String> = std::env::args().collect(); 
+///     if args.len() < 3 {
+///         eprintln!("Usage: <numbers...> <K>");
+///         std::process::exit(1);
+///     }
+///     // K는 마지막 인자
+///     let k: usize = args.last().unwrap().parse().expect("K must be a number");
+///     // ...
+///     let result = max_even_sum(a, k);
+///     println!("{}", result);
+/// }
+/// ```
 fn main() {
     let args: Vec<String> = std::env::args().collect();
 
     if args.len() < 3 {
+        // eprintln!() 오직 오류일때 사용        
         eprintln!("Usage: <numbers...> <K>");
         std::process::exit(1);
     }
@@ -15,12 +35,13 @@ fn main() {
     // K는 마지막 인자
     let k: usize = args.last().unwrap().parse().expect("K must be a number");
 
-    // 배열은 나머지 인자
-    let a: Vec<i32> = args[1..args.len()-1]
+    // 배열은 나머지 인자 <i32> 형타입은 숫자 기본.
+    let a: Vec<i32> = args[1..args.len()-1] // args[1..] 표시는 배열 시작과 끝 표시
         .iter()
         .map(|s| s.parse().expect("All numbers must be integers"))
         .collect();
 
     let result = max_even_sum(a, k);
+    // println!() 는 표준 출력, "{}" 는 외부 인자를 받아 출력
     println!("{}", result);
 }
